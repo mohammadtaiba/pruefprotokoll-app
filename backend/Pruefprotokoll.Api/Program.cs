1fefba6f2fd90acd9c72c4d5bbfeb8e9ad2d5c57
Mohammad Taiba <mohammadtaiba54@gmail.com>
2026-06-24 12:00:54 +0200

    Rename backend namespace to Pruefprotokoll

---
+++ backend/Pruefprotokoll.Api/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Pruefprotokoll.Api.Data;
+using Pruefprotokoll.Api.Repositories;
+using Pruefprotokoll.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IInspectionReportRepository, InspectionReportRepository>();
+builder.Services.AddScoped<IInspectionReportService, InspectionReportService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularDevClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+app.UseCors("AngularDevClient");
+app.MapControllers();
+
+app.Run();
